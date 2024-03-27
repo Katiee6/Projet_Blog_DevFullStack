5b16678bb732773c5e467666c359df72fc44f4a7
Katiee6 <katia.bouarab@toulouse.miage.fr>
2024-03-27 09:10:44 +0100

    modif globale + (profil pas fini)

--- blog/src/app/app.routes.ts
+++ blog/src/app/app.routes.ts
@@ -6,15 +6,17 @@ import {BlogComponent} from "./blog/blog.component";
 import {NouveauMessageComponent} from "./nouveau-message/nouveau-message.component";
 import {ListeMessagesComponent} from "./liste-messages/liste-messages.component";
 import {ProfilComponent} from "./profil/profil.component";
+import {FooterComponent} from "./footer/footer.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 export const routes: Routes = [
   { path: 'connexion', component: ConnexionComponent },
   { path: 'creer-compte', component: CreerCompteComponent },
   { path: 'profil', component: ProfilComponent },
-  //{ path: 'blog', component: BlogComponent }, // ?? supprimer blog ??
+  { path: 'blog', component: BlogComponent },
   { path: 'nouveau-message', component: NouveauMessageComponent },
   { path: 'liste-messages', component: ListeMessagesComponent },
   { path: 'message/:id', component: MessageComponent }, //Spécifique à un message
-  { path : '', redirectTo : 'connexion', pathMatch :'full'},
-  { path: '**', redirectTo: 'connexion' }, // Vers Connexion ou Blog ???
+  { path : '', redirectTo : 'blog', pathMatch :'full'},
+  { path: '**', redirectTo: 'blog' },
 ];
